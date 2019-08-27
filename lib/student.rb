5e6a17530aca37e2a559b1155bc65b34355ad58a
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-27 05:09:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,12 @@ class Student
     
     def self.students_below_12th_grade 
       sql = <<-SQL
-        SELECT 
+        SELECT * 
+        FROM students 
+        WHERE grade < 12; 
+      SQL
+      
+      
       
     end 
     
