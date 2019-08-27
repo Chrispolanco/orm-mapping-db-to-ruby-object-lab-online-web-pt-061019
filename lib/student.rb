6fcf11aa285854c06c301246f4cb62613316b49a
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-27 05:15:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,9 +83,14 @@ class Student
     end
   end 
   
-  def self.first_x_students_in_grade_10 
+  def self.first_x_students_in_grade_10(num)
     sql = <<-SQL 
-      SELECT 
+      SELECT * 
+      FROM students 
+      Where grade = 10; 
+    SQL
+    
+    
     
   end 
   
